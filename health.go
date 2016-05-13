29a39e489e1541a33cb48f11bfb3bc1115aa113e
Jesse Millar <hellojessemillar@gmail.com>
2016-05-13 13:48:04 -0600

    Update health.go

--- health.go
+++ health.go
@@ -8,5 +8,5 @@ import (
 
 // Check returns a message indicating that the service is awake and listening
 func Check(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
+	return c.JSON(http.StatusOK, "Uh, we had a slight weapons malfunction, but uh...everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
 }
