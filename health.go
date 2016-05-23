e577f5aa2033dd0aee8885d408b8a6b13a187ef2
Jesse Millar <hellojessemillar@gmail.com>
2016-05-23 11:26:22 -0600

    Adding the jsonresp package

--- health.go
+++ health.go
@@ -3,10 +3,11 @@ package health
 import (
 	"net/http"
 
+	"github.com/jessemillar/jsonresp"
 	"github.com/labstack/echo"
 )
 
 // Check returns a message indicating that the service is awake and listening
-func Check(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Uh, we had a slight weapons malfunction, but uh...everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
+func Check(context echo.Context) error {
+	return jsonresp.Create(context, http.StatusOK, "Uh, we had a slight weapons malfunction, but uh...everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
 }
