b57b0f091e7065010c30f591c7ba96cb8769aa83
Jesse Millar <hellojessemillar@gmail.com>
2016-06-14 14:49:35 -0600

    Fixing jsonresp naming

--- health.go
+++ health.go
@@ -9,5 +9,5 @@ import (
 
 // Check returns a message indicating that the service is awake and listening
 func Check(context echo.Context) error {
-	return jsonresp.Create(context, http.StatusOK, "Uh, we had a slight weapons malfunction, but uh...everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
+	return jsonresp.New(context, http.StatusOK, "Uh, we had a slight weapons malfunction, but uh...everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
 }
